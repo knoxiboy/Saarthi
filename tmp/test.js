f2a2d66b624e128e6f87fca388bd7afc887e2b93
knoxiboy <karankmt.tripathi@gmail.com>
2026-03-07 21:40:09 +0530

    Cleanup temporary benchmark and test scripts

--- tmp/test.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:3000/api/mock-interview/start", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ type: "Custom", topic: "React", duration: "15 min", difficulty: "Medium" })
-}).then(res => res.text()).then(console.log).catch(console.error);
